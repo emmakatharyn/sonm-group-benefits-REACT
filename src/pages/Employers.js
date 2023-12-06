303c10a98a23b8fba277160081f539fa68931e7f
Emma Reed <ereed@easitpa.com>
2023-12-05 21:36:11 -0700

    create pages for Employer Resources & Enroll Here

---
+++ src/pages/Employers.js
@@ -0,0 +1,341 @@
+function Employers() {
+  return (
+    <>
+      <div class='container'>
+        <h1 class='my-4 mb-2 display-5 text-center'>
+          Employer Information Resources
+        </h1>
+      </div>
+      <div class='container my-3 mx-auto'>
+        <div class='side-by-side'>
+          <div class='list-box'>
+            <h4 class='my-2 py-2'>POLICIES, DOCUMENTS AND FORMS</h4>
+            <ul>
+              <li>
+                <a
+                  href='documents/Administrative_Guide_2023_July_Final-edit-2.pdf'
+                  target='_blank'
+                >
+                  {" "}
+                  Administrative Guide
+                </a>
+              </li>
+              <li>
+                <a
+                  href='AdministrativeLetters.html'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  {" "}
+                  Administrative Letters
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/Documents/Benefits_Eligibility_Acknowledgement_2023.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Benefits Eligibility Acknowledgement Form (English)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/Documents/Benefits_Eligibility_Acknowledgement_2023_Spanish.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Benefits Eligibility Acknowledgement Form (Espa&ntilde;ol)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/documents/Benefits_Eligibility_Acknowledgement_2023_Waiving_All_Benefits.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Benefits Eligibility Acknowledgement Form - Waiving Benefits
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/documents/Benefits_Eligibility_Acknowledgement_2023_Waiving_All_Benefits_Spanish.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Benefits Eligibility Acknowledgement Form - Waiving Benefits
+                  (Espa&ntilde;ol)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/Documents/COBRA_Notification_Form-Revised_6-8-2021.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  COBRA Notification to Term Benefits
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/documents/RMD_Current_Year_Refund_Request_Employee_Revised_2021.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  RMD Current Year Refund Request (Employee)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/documents/RMD_Current_Year_Refund_Request_ER-financial_Agency_(1)_(002).pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  RMD Current Year Refund Request (Employer)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/documents/State_of_New_Mexico_Life_Claim_Form.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Death Claim Form
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/documents/DFA-Summary_Page_2020.xls'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  DFA Summary Page (Download)
+                </a>
+              </li>
+              <li>
+                <a href='./Disability.html' target='_blank' rel='noreferrer'>
+                  Disability Policy
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/documents/Disability Calculation Sheet-Final.xlsx'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Disability Calculation Sheet (Download)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='./employerResourcesSub1.html'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Domestic Partnership Form
+                </a>
+              </li>
+              <li>
+                <a
+                  href='./employeePortal.html'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Employee Enrollment Resources
+                </a>
+              </li>
+              <li>
+                <a href='./PremiumsDue.html' target='_blank' rel='noreferrer'>
+                  LWOP Benefit Premium Transmittal Form
+                </a>
+              </li>
+              <li>
+                <a
+                  href='documents/Notification_to_Term_Benefits_July2022.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Notification to Terminate Benefits Due to Non-Payment of
+                  Premiums
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/Documents/POP-SoNM-NOTICE-OF-WAIVER-2023.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  POP Waiver Form (State)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/PremiumRatesSAE.html'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  PREMIUM RATE SHEETS – STATE
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/Documents/RMD_Prior_Year_Refund_Request_Employee_Revised_2021.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  RMD Prior Calendar Year Refund Request Form (Employee)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/Documents/RMD_Prior_Year_Refund_Request_ER-financial_Agency_2.6.2020.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  RMD Prior Calendar Year Refund Request Form (Agency)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/documents/W-9_Example.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  W-9 Form Instructions
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/documents/W-9_New_Form.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  W-9 New Form
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/documents/Premium Payment OPR Transmittal.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Premium Payment OPR Transmittal
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='list-box'>
+            <h4 class='my-2 py-2'>NOTICES, RESOURCES AND INFORMATION</h4>
+            <ul>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/Documents/Benefit_Comparison_Grid-FY21_Final(003).pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Benefits Comparison Guide 2021
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/Documents/Benefit_Comparison_Grid_FY22_Final_v2.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Benefits Comparison Guide 2022
+                </a>
+              </li>
+              <li>
+                <a href='./BenefitsInformation.html'>Benefits Information</a>
+              </li>
+              <li>
+                <a href='./CarrierContacts.html'>Benefit Plan Contacts</a>
+              </li>
+              <li>
+                <a href='./ViewPremiumCal.html'>Benefits Premium Calculator</a>
+              </li>
+              <li>
+                <a href='./COBRA.html'>COBRA Resources Page</a>
+              </li>
+              <li>
+                <a href='./Disability.html'>Disability Resources Page</a>
+              </li>
+              <li>
+                <a href='./FGPOpenFPN.html'>
+                  Federal Medicare Part D Creditable Coverage Notices
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/Documents/HIPAA_Privacy_Notice.PDF'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  HIPAA Privacy Notice
+                </a>
+              </li>
+              <li>
+                <a href='./HRresources.html' target='_blank'>
+                  HR Meetings and Recordings
+                </a>
+              </li>
+              <li>
+                <a href='./news.html' target='_blank'>
+                  Newsletter Archive
+                </a>
+              </li>
+              <li>
+                <a href='./employeePortalSub3.html' target='_blank'>
+                  SHARE Self Service Instruction Guide
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/documents/Emply_Trifold_6_28_7_18.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Trifold Benefits Brochure
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='list-box'>
+            <h4 class='my-2 py-2'>LPB FORMS</h4>
+            <ul>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/documents/LPB_Change_Form.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  LPB Employee Change/Correction Form
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/documents/POP-LPB-NOTICE-OF-WAIVER-2023.pdf'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  POP Waiver Form (LPB)
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.mybenefitsnm.com/rmdPremiumRates.html'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Premium Rates Sheets – LPB
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Employers;
