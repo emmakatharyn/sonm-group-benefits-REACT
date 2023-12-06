18f1644f08a3318c6c486824181a507571ec7907
Emma Reed <ereed@easitpa.com>
2023-12-05 21:38:44 -0700

    change <a>'s to <Link>'s in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,24 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
       <div className='container erisa-credits'>
-        <a href='#'>
+        <Link to='/'>
           <img src='images/logo.png' alt='State Of New Mexico Group logo' />
           State Of New Mexico Group Benefits Plan
-        </a>
+        </Link>
         <div className='easi'>
           <a href='BenefitsInformation.html' target='_blank'>
             <img src='images/easi.png' width='118' alt='Erisa logo' />
           </a>
-          <a href='https://www.erisatrust.com/' target='_blank'>
+          <Link to='https://www.erisatrust.com/' target='_blank'>
             <img
               src='images/tree.png'
               width='72'
               alt='Erisa trust company logo'
             />
-          </a>
+          </Link>
         </div>
       </div>
     </footer>
