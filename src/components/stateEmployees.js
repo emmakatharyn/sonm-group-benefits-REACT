b5233daa8931d02b87997775be31980872d19b54
Emma Reed <ereed@easitpa.com>
2023-12-08 21:37:24 -0700

    set up Links and create documents directory

--- src/components/stateEmployees.js
+++ src/components/stateEmployees.js
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 function StateEmployees() {
   return (
     <div class='container portal-grid my-5' id='state-ee-grid'>
-      <a href='StateAgencyEmployee.html' class='btn btn-primary quicklink'>
+      <Link to='/state-agency-employees' class='btn btn-primary quicklink'>
         <i class='bx bxs-folder-plus'></i>
         <br />I am a New Hire
-      </a>
-      <a href='employeePortalSub2.html' class='btn btn-primary quicklink'>
+      </Link>
+      {/* previously linked to employeeportalsub2 */}
+      <Link to='/qualifying-event' class='btn btn-primary quicklink'>
         <i class='bx bxs-calendar-exclamation'></i>
         <br />I have a Qualifying Event
-      </a>
+      </Link>
 
-      <a
-        href='#'
+      {/* modal here */}
+      <button
+        href=''
         data-bs-target='#newHireOrientation'
         data-bs-toggle='modal'
         class='btn btn-primary quicklink'
@@ -19,36 +23,40 @@ function StateEmployees() {
         <i class='bx bxs-info-circle'></i>
         <br />
         New Hire & Qualifying Event Orientation
-      </a>
-      <a href='benefitsInformation.html' class='btn btn-primary quicklink'>
+      </button>
+
+      {/* benefitsinformation.html */}
+      <Link to='/benefits-info' class='btn btn-primary quicklink'>
         <i class='bx bxs-file-doc'></i>
         <br />
         Benefits Information
-      </a>
-      <a href='employeePortalSub3.html' class='btn btn-primary quicklink'>
+      </Link>
+
+      <Link to='/share' class='btn btn-primary quicklink'>
         <i class='bx bx-book-open'></i>
         <br />
         SHARE Self Service Manual
-      </a>
-      <a href='AdministrativeLetters.html' class='btn btn-primary quicklink'>
+      </Link>
+      <Link to='/admin-letters' class='btn btn-primary quicklink'>
         <i class='bx bxs-star'></i>
         <br />
         Special Rules: LWOP, FMLA and More
-      </a>
-      <a href='employerResourcesSub1.html' class='btn btn-primary quicklink'>
+      </Link>
+      <Link to='/domestic-partnership' class='btn btn-primary quicklink'>
         <i class='bx bxs-file-pdf'></i>
         <br />
         Domestic Partnership Forms
-      </a>
-      <a
-        href='/documents/POP-SoNM-NOTICE-OF-WAIVER-2023.pdf'
+      </Link>
+      <Link
+        to='/documents/Aetna_Network_Flyer_proof_2.pdf'
+        // to='/documents/POP-SoNM-NOTICE-OF-WAIVER-2023.pdf'
         class='btn btn-primary quicklink'
         target='_blank'
       >
         <i class='bx bxs-pen'></i>
         <br />
         POP Waiver Form (State)
-      </a>
+      </Link>
     </div>
   );
 }
