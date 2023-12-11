af513f3b2ec467b239d2009af6fe3e1646aa201d
Emma Reed <ereed@easitpa.com>
2023-12-10 17:06:36 -0700

    change anchor tags to Link components

--- src/pages/Employers.js
+++ src/pages/Employers.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 function Employers() {
   return (
     <>
@@ -12,215 +13,214 @@ function Employers() {
             <h4 class='my-2 py-2'>POLICIES, DOCUMENTS AND FORMS</h4>
             <ul>
               <li>
-                <a
-                  href='documents/Administrative_Guide_2023_July_Final-edit-2.pdf'
+                <Link
+                  to='documents/Administrative_Guide_2023_July_Final-edit-2.pdf'
                   target='_blank'
                 >
-                  {" "}
                   Administrative Guide
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='AdministrativeLetters.html'
+                <Link
+                  to='AdministrativeLetters.html'
                   target='_blank'
                   rel='noreferrer'
                 >
                   {" "}
                   Administrative Letters
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/Documents/Benefits_Eligibility_Acknowledgement_2023.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/Documents/Benefits_Eligibility_Acknowledgement_2023.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Benefits Eligibility Acknowledgement Form (English)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/Documents/Benefits_Eligibility_Acknowledgement_2023_Spanish.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/Documents/Benefits_Eligibility_Acknowledgement_2023_Spanish.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Benefits Eligibility Acknowledgement Form (Espa&ntilde;ol)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/documents/Benefits_Eligibility_Acknowledgement_2023_Waiving_All_Benefits.pdf'
+                <Link
+                  to='/documents/Benefits_Eligibility_Acknowledgement_2023_Waiving_All_Benefits.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Benefits Eligibility Acknowledgement Form - Waiving Benefits
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/documents/Benefits_Eligibility_Acknowledgement_2023_Waiving_All_Benefits_Spanish.pdf'
+                <Link
+                  to='/documents/Benefits_Eligibility_Acknowledgement_2023_Waiving_All_Benefits_Spanish.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Benefits Eligibility Acknowledgement Form - Waiving Benefits
                   (Espa&ntilde;ol)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/Documents/COBRA_Notification_Form-Revised_6-8-2021.pdf'
+                <Link
+                  to='/Documents/COBRA_Notification_Form-Revised_6-8-2021.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   COBRA Notification to Term Benefits
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/documents/RMD_Current_Year_Refund_Request_Employee_Revised_2021.pdf'
+                <Link
+                  to='/documents/RMD_Current_Year_Refund_Request_Employee_Revised_2021.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   RMD Current Year Refund Request (Employee)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/documents/RMD_Current_Year_Refund_Request_ER-financial_Agency_(1)_(002).pdf'
+                <Link
+                  to='/documents/RMD_Current_Year_Refund_Request_ER-financial_Agency_(1)_(002).pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   RMD Current Year Refund Request (Employer)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/documents/State_of_New_Mexico_Life_Claim_Form.pdf'
+                <Link
+                  to='/documents/State_of_New_Mexico_Life_Claim_Form.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Death Claim Form
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/documents/DFA-Summary_Page_2020.xls'
+                <Link
+                  to='/documents/DFA-Summary_Page_2020.xls'
                   target='_blank'
                   rel='noreferrer'
                 >
                   DFA Summary Page (Download)
-                </a>
+                </Link>
               </li>
               <li>
-                <a href='./Disability.html' target='_blank' rel='noreferrer'>
+                <Link to='./Disability.html' target='_blank' rel='noreferrer'>
                   Disability Policy
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/documents/Disability Calculation Sheet-Final.xlsx'
+                <Link
+                  to='/documents/Disability Calculation Sheet-Final.xlsx'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Disability Calculation Sheet (Download)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='./employerResourcesSub1.html'
+                <Link
+                  to='./employerResourcesSub1.html'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Domestic Partnership Form
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='./employeePortal.html'
+                <Link
+                  to='./employeePortal.html'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Employee Enrollment Resources
-                </a>
+                </Link>
               </li>
               <li>
-                <a href='./PremiumsDue.html' target='_blank' rel='noreferrer'>
+                <Link to='./PremiumsDue.html' target='_blank' rel='noreferrer'>
                   LWOP Benefit Premium Transmittal Form
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='documents/Notification_to_Term_Benefits_July2022.pdf'
+                <Link
+                  to='documents/Notification_to_Term_Benefits_July2022.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Notification to Terminate Benefits Due to Non-Payment of
                   Premiums
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/Documents/POP-SoNM-NOTICE-OF-WAIVER-2023.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/Documents/POP-SoNM-NOTICE-OF-WAIVER-2023.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   POP Waiver Form (State)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/PremiumRatesSAE.html'
+                <Link
+                  to='https://www.mybenefitsnm.com/PremiumRatesSAE.html'
                   target='_blank'
                   rel='noreferrer'
                 >
                   PREMIUM RATE SHEETS – STATE
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/Documents/RMD_Prior_Year_Refund_Request_Employee_Revised_2021.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/Documents/RMD_Prior_Year_Refund_Request_Employee_Revised_2021.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   RMD Prior Calendar Year Refund Request Form (Employee)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/Documents/RMD_Prior_Year_Refund_Request_ER-financial_Agency_2.6.2020.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/Documents/RMD_Prior_Year_Refund_Request_ER-financial_Agency_2.6.2020.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   RMD Prior Calendar Year Refund Request Form (Agency)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/documents/W-9_Example.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/documents/W-9_Example.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   W-9 Form Instructions
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/documents/W-9_New_Form.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/documents/W-9_New_Form.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   W-9 New Form
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='/documents/Premium Payment OPR Transmittal.pdf'
+                <Link
+                  to='/documents/Premium Payment OPR Transmittal.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Premium Payment OPR Transmittal
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -228,75 +228,79 @@ function Employers() {
             <h4 class='my-2 py-2'>NOTICES, RESOURCES AND INFORMATION</h4>
             <ul>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/Documents/Benefit_Comparison_Grid-FY21_Final(003).pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/Documents/Benefit_Comparison_Grid-FY21_Final(003).pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Benefits Comparison Guide 2021
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/Documents/Benefit_Comparison_Grid_FY22_Final_v2.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/Documents/Benefit_Comparison_Grid_FY22_Final_v2.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Benefits Comparison Guide 2022
-                </a>
+                </Link>
               </li>
               <li>
-                <a href='./BenefitsInformation.html'>Benefits Information</a>
+                <Link to='./BenefitsInformation.html'>
+                  Benefits Information
+                </Link>
               </li>
               <li>
-                <a href='./CarrierContacts.html'>Benefit Plan Contacts</a>
+                <Link to='./CarrierContacts.html'>Benefit Plan Contacts</Link>
               </li>
               <li>
-                <a href='./ViewPremiumCal.html'>Benefits Premium Calculator</a>
+                <Link to='./ViewPremiumCal.html'>
+                  Benefits Premium Calculator
+                </Link>
               </li>
               <li>
-                <a href='./COBRA.html'>COBRA Resources Page</a>
+                <Link to='./COBRA.html'>COBRA Resources Page</Link>
               </li>
               <li>
-                <a href='./Disability.html'>Disability Resources Page</a>
+                <Link to='./Disability.html'>Disability Resources Page</Link>
               </li>
               <li>
-                <a href='./FGPOpenFPN.html'>
+                <Link to='./FGPOpenFPN.html'>
                   Federal Medicare Part D Creditable Coverage Notices
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/Documents/HIPAA_Privacy_Notice.PDF'
+                <Link
+                  to='https://www.mybenefitsnm.com/Documents/HIPAA_Privacy_Notice.PDF'
                   target='_blank'
                   rel='noreferrer'
                 >
                   HIPAA Privacy Notice
-                </a>
+                </Link>
               </li>
               <li>
-                <a href='./HRresources.html' target='_blank'>
+                <Link to='./HRresources.html' target='_blank'>
                   HR Meetings and Recordings
-                </a>
+                </Link>
               </li>
               <li>
-                <a href='./news.html' target='_blank'>
+                <Link to='./news.html' target='_blank'>
                   Newsletter Archive
-                </a>
+                </Link>
               </li>
               <li>
-                <a href='./employeePortalSub3.html' target='_blank'>
+                <Link to='./employeePortalSub3.html' target='_blank'>
                   SHARE Self Service Instruction Guide
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/documents/Emply_Trifold_6_28_7_18.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/documents/Emply_Trifold_6_28_7_18.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Trifold Benefits Brochure
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -304,31 +308,31 @@ function Employers() {
             <h4 class='my-2 py-2'>LPB FORMS</h4>
             <ul>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/documents/LPB_Change_Form.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/documents/LPB_Change_Form.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   LPB Employee Change/Correction Form
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/documents/POP-LPB-NOTICE-OF-WAIVER-2023.pdf'
+                <Link
+                  to='https://www.mybenefitsnm.com/documents/POP-LPB-NOTICE-OF-WAIVER-2023.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
                   POP Waiver Form (LPB)
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href='https://www.mybenefitsnm.com/rmdPremiumRates.html'
+                <Link
+                  to='https://www.mybenefitsnm.com/rmdPremiumRates.html'
                   target='_blank'
                   rel='noreferrer'
                 >
                   Premium Rates Sheets – LPB
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
