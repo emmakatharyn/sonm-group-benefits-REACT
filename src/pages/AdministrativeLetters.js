80b84eceef0ca411fd8e1cff73bd5f62dbd2d801
Emma Reed <ereed@easitpa.com>
2023-12-28 10:59:04 -0700

    add boxicons cdn tag

--- src/pages/AdministrativeLetters.js
+++ src/pages/AdministrativeLetters.js
@@ -12,7 +12,7 @@ function AdministrativeLetters() {
           <div class='list-box'>
             <ul>
               <li>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='documents/Administrative_Guide_2023_July_Final-edit-2.pdf'
                   target='_blank'
@@ -21,19 +21,17 @@ function AdministrativeLetters() {
                 </Link>
               </li>
               <li>
-                <i class='bx bxs-file-pdf'></i>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='documents/20210608094443318.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
-                  {" "}
                   Benefits Administration Letter 21-001
                 </Link>
               </li>
               <li>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='documents/14-003_BenAdminltr_Addl_Administrative_Processes_for_LPBs_SIGNED.pdf'
                   target='_blank'
@@ -43,7 +41,7 @@ function AdministrativeLetters() {
                 </Link>
               </li>
               <li>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='documents/14-002_BenAdminLtr_LPB_Premium_Load_changes_SIGNED.pdf'
                   target='_blank'
@@ -53,7 +51,7 @@ function AdministrativeLetters() {
                 </Link>
               </li>
               <li>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='/documents/14-001_BenAdminltr_Admin_Changes_to_Disability_Program_SIGNED.pdf'
                   target='_blank'
@@ -63,7 +61,7 @@ function AdministrativeLetters() {
                 </Link>
               </li>
               <li>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='/documents/13-004_BenAdminltr_LPB_Disability_Checks_Taxes_SIGNED.pdf'
                   target='_blank'
@@ -73,7 +71,7 @@ function AdministrativeLetters() {
                 </Link>
               </li>
               <li>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='/Documents/13-003_BenAdminltr_COBRA_Billing_Changes_SIGNED.pdf'
                   target='_blank'
@@ -83,7 +81,7 @@ function AdministrativeLetters() {
                 </Link>
               </li>
               <li>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='/documents/13-002_BenAdminltr_LPB_Selfpay_Billing_Changes_SIGNED.pdf'
                   target='_blank'
@@ -93,7 +91,7 @@ function AdministrativeLetters() {
                 </Link>
               </li>
               <li>
-                <i class='bx bxs-file-pdf'></i>
+                <i class='bx bxs-file-pdf'></i>{" "}
                 <Link
                   to='/documents/13-001_BenAdminltr_LPB_Separation_Process_SIGNED.pdf'
                   target='_blank'
